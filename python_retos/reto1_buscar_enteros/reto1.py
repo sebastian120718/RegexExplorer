812440e5d6346b741ddcbadc5761c80d5bf957ad
sebastian120718 <jsgarciag2@academia.usbbog.edu.co>
2025-08-25 20:46:06 +0000

    primer commir

--- python_retos/reto1_buscar_enteros/reto1.py
+++ python_retos/reto1_buscar_enteros/reto1.py
@@ -10,7 +10,7 @@ import re
 texto = "En 2023, había 15 estudiantes y 3 profesores."
 
 # Expresión regular para enteros (positivos y negativos)
-patron = r"-?\\b\\d+\\b"
+patron = r"-?\b\d+\b"
 
 # Buscar todos los enteros
 enteros = re.findall(patron, texto)
